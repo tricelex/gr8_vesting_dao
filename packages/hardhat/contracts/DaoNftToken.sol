d0daf2b5acfa90d043fa7a463d6a9a999eb0c12f
emmanuelokoye <emmanuelokoye@quickcheck.ng>
2022-05-04 22:33:54 +0100

    created nft contract

---
+++ packages/hardhat/contracts/DaoNftToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC721Checkpointable} from "./ERC721Checkpointable.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract DaoNftToken is Ownable, ERC721Checkpointable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("DAO NFT", "DAFT") {}
+
+    function safeMint(address to) public onlyOwner returns () {
+        _safeMint(to, _tokenIdCounter.current());
+        _tokenIdCounter.increment();
+    }
+}
